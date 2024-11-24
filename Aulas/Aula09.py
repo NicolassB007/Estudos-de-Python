99aea87ec8602779323e6324eaed7cd734baf4dc
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2024-11-24 15:41:58 -0300

    .

---
+++ Aulas/Aula09.py
@@ -0,0 +1,25 @@
+adicao = 10 + 10
+print("Adição:", adicao)
+
+subtracao = 10 - 5
+print("Subtração:", subtracao)
+
+multiplicacao = 10 * 10
+print("Multiplicação:", multiplicacao)
+
+divisao = 10 / 2.2 # sempre retorna um valor float
+print("Divisão:", divisao)
+
+divisao_inteira = 10 // 2.2 
+print("Divisão inteira:", divisao_inteira)
+
+exponenciacao = 2 ** 10
+print("Exponenciação:", exponenciacao)
+
+modulo = 55 % 2 # Resto da divisão
+print("Módulo:", modulo)
+
+print(10 % 8 == 0) # Checagem para saber se 10 é multiplo de 8
+print(16 % 8 == 0) # Checagem para saber se 16 é divisivel por 8
+print(10 % 2 == 0) # Checagem para saber se 10 é par
+print(15 % 2 == 0) # Checagem para saber se 15 é par ou ímpar
---
+++ Exercícios/Ex01-Aula08.py
@@ -0,0 +1,13 @@
+# Ex 01 - Aula 8
+nome = "Nicolas"
+sobrenome = "Bissotto"
+idade = 17
+ano_nascimento = 2007
+maior_idade = idade >= 18
+altura_metros = 1.81
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de nascimento:", ano_nascimento)
+print("É maior de idade?", maior_idade)
+print("Altura em metros:", altura_metros)
