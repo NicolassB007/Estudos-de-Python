8a3f35aabf3bb0bf002485f44d10d2be772718ba
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2024-11-24 02:12:34 -0300

    .

--- Aula04.py
+++ Aula04.py
@@ -3,9 +3,21 @@
 # O tipo int representa qualquer número
 # Positivo ou negativo. int sem sinal é considerado positivo
 
-# Float -> Número com ponto flutuante
-# O tipo float representa qualquer número
-# Positivo ou negativo com ponto flutuante
-# O float sem sinal é considerado positivo
+# print(1231) # int
+# print(129) # int
+# print(-2) # int
+# print(0) # int
+
+# # Float -> Número com ponto flutuante
+# # O tipo float representa qualquer número
+# # Positivo ou negativo com ponto flutuante
+# # O float sem sinal é considerado positivo
+
+# print(1.1, 11.3) # float
+# print(0.0) # float
+# print(-2.4) # float
 
 # A função type mostra o tipo que o Python inferiu ao valor
+print(type("Nicolas"))
+print(type(-12))
+print(type(1.4))
