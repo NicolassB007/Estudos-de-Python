5a62f37bbd59ae28d30c9aa6edb4aa0749ceea68
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2024-11-25 23:52:21 -0300

    .

---
+++ Aulas/Aula15.py
@@ -0,0 +1,10 @@
+# nome = input("Qual o seu nome? ")
+# print(f"O seu nome é {nome}")
+
+valor = input("Primeiro valor: ")
+valor_2 = input("Segundo valor: ")
+
+int_valor = int(valor)
+int_valor_2 = int(valor_2)
+
+print(f"A soma dos números é {int_valor + int_valor_2}")
