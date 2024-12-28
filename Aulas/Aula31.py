02c2b6bfb0171b6179a69caafe511ced89f05dac
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2024-12-28 02:56:42 -0300

    ..

--- Aulas/Aula31.py
+++ Aulas/Aula31.py
@@ -4,16 +4,15 @@ None - Não valor
 is e is not = é ou não é (tipo, valor, identidade)
 id = Identidade
 """
-v1 = 'a'
-v2 = 'a'
-v3 = 'b'
-print(id(v1))
-print(id(v2))
-print(id(v3))
 
-# condicao = False
+condicao = True
+passou_no_if = None
 
-# if condicao:
-    # print("Faça algo")
-# else:
-    #print("Não faça algo")
+if condicao:
+    passou_no_if = True
+    print("Faça algo")
+else:
+    print("Não faça algo")
+
+print(passou_no_if, passou_no_if is None)
+print(passou_no_if, passou_no_if is not None)
