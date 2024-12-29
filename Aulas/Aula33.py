4dfd9e54581865badcf49dd66a564de34908e576
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2024-12-29 19:45:31 -0300

    ..

---
+++ Aulas/Aula33.py
@@ -0,0 +1,10 @@
+"""
+Tipo built-in - Tipo embutido, que vem detro do próprio Python]
+Não é preciso instalar nada
+"""
+
+string = 'nicolas bissotto'
+outra_variavel = f"{string[:6]}123{string[7:]}"
+print(string)
+print(outra_variavel)
+print(string.capitalize())
