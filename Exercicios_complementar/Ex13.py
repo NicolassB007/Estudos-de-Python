12ac34d00d24427eb949cab67528dcfd30541e8d
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2025-01-04 12:00:23 -0300

    ..

--- Exercicios_complementar/Ex13.py
+++ Exercicios_complementar/Ex13.py
@@ -1,11 +1,11 @@
-print(10 * "=-")
-print("FIBONACCHI".center(18))
-print(10 * "=-")
-# Em andamento
+print(12 * "=-")
+print("FIBONACCHI".center(22))
+print(12 * "=-")
 
 valor = input("Informe um valor: ")
 valor_int = 0
 valor_valido = None
+
 try:
     valor_int = int(valor)
     valor_valido = True
@@ -13,12 +13,13 @@ except:
     valor_valido = None
 
 contador = 0
+valor_anterior = 0
 proximo_valor = 1
-while contador < valor_int:
-    valor_anterior = ...
-    fibonacchi = valor_anterior + proximo_valor
-    proximo_valor = fibonacchi
-    contador += 1
-    print(fibonacchi)
 
-
+if valor_valido is True:
+    while contador < valor_int: 
+        fibonacchi = valor_anterior + proximo_valor
+        valor_anterior = proximo_valor
+        proximo_valor = fibonacchi
+        print(fibonacchi)
+        contador += 1
