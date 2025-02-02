ee187d88e6c925b448ac48e9b26f17308dc7d755
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2025-02-01 22:36:46 -0300

    Perguntas e respostas
    Feito com auxilio do professor do curso

--- Exercícios/Ex20-Aula70.py
+++ Exercícios/Ex20-Aula70.py
@@ -1,5 +1,5 @@
 # Exercício - Sistema de perguntas e respostas
-# Em andamento
+# Feito com auxilio do professor do curso online UDEMY
 
 perguntas = [
     {
@@ -18,3 +18,38 @@ perguntas = [
         "Resposta": '5',  
     }
 ]
+
+qtd_acertos = 0
+for pergunta in perguntas:
+    print("Pergunta: ", pergunta["Pergunta"])
+
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
+        print(f"{i}) {opcao}")
+
+    esc_user = input("Informe uma opção: ")
+
+    qtd_options = len(opcoes)
+    acerto = False
+    esc_user_int = 0
+    esc_user_valid = None
+
+    if esc_user.isdigit():
+        esc_user_int = int(esc_user)
+        esc_user_valid = True
+
+    if esc_user_valid is True:
+        if esc_user_int >= 0 and esc_user_int < qtd_options:
+            if opcoes[esc_user_int] == pergunta["Resposta"]:
+                acerto = True
+                qtd_acertos += 1
+        
+    if acerto:
+        print("Parabéns, você acertou!")
+    else:
+        print("Que pena, você errou!")
+
+    print('\n')
+        
+print(f"Você teve {qtd_acertos} acertos!")
+print(f"ACERTOS {qtd_acertos} de {len(perguntas)}")
