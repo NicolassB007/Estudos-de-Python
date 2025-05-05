7c839152bd30539131875f68f47756c1ba1014f0
Nicolas Bissotto da Silva <nicolasbissottomeusestudos@gmail.com>
2025-05-04 21:25:48 -0300

    List Comprehension, Mapping

---
+++ Aulas/Aula82.py
@@ -0,0 +1,27 @@
+# List comprehension em Python
+# List comprehension é uma forma rápida de criar listas
+# a partir de iteráveis
+
+# print(list(range(10)))
+
+# lista = []
+
+# for numero in range(10):
+#     lista.append(numero)
+# print(lista)
+
+# lista = [valor * 2 for valor in range(10)]
+# print(lista)
+
+# Mapeamento de dados em list comprehension
+produtos = [
+    {"nome": "produto01", "preco": 20},
+    {"nome": "produto02", "preco": 10},
+    {"nome": "produto03", "preco": 30},
+]
+
+novos_produtos = [
+    {**produto, "preço": produto["preco"] * 1.05} if produto["preco"] > 20 else {**produto} for produto in produtos
+]
+
+print(*novos_produtos, sep="\n")
